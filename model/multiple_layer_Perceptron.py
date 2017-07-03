c2d2e1920740d811018b777c0337c2bd9fe45cb7
xyan <d>
2017-07-03 17:24:21 +0800

    modify model

--- model/multiple_layer_Perceptron.py
+++ model/multiple_layer_Perceptron.py
@@ -5,6 +5,10 @@ import pickle
 import logging
 
 
+
+
+earth_radius = 6.3781e6
+
 flags = tf.app.flags
 FLAGS = flags.FLAGS
 flags.DEFINE_float("bn_epsilon", 1e-5, "The epsilon of batch normalization")
@@ -29,10 +33,6 @@ def full_connect(inputs,weight_shape,biase_shape,layer_name):
             layer = tf.nn.dropout(layer, FLAGS.dropout_keep_prob)
             tf.summary.histogram('dropout_output', layer)
 
-        with tf.name_scope(layer_name + 'output_relu'):
-            layer = tf.nn.relu(layer)
-            tf.summary.histogram('relu_output', layer)
-
         with tf.name_scope(layer_name + 'batch_normalize'):
             mean, var = tf.nn.moments(layer, axes=[0])
             scale = tf.Variable(tf.truncated_normal([biase_shape,],stddev=0.01,dtype=tf.float64))
@@ -42,6 +42,12 @@ def full_connect(inputs,weight_shape,biase_shape,layer_name):
             layer = tf.nn.batch_normalization(layer, mean, var, shift, scale,
                                               FLAGS.bn_epsilon)
             tf.summary.histogram('batch_normalizer', layer)
+
+        with tf.name_scope(layer_name + 'output_relu'):
+            layer = tf.nn.relu(layer)
+            tf.summary.histogram('relu_output', layer)
+
+
 
 
     return layer
@@ -81,26 +87,45 @@ def construct_graph(x,center_point):
 
     return output
 
-def generate_random_batch(data,label,batch_size):
+def generate_train_random_batch(data,label,batch_size,is_train = True):
 
     indics = np.random.randint(0,len(data),size=batch_size)
     data_batch = data.iloc[indics]
-    label_batch = label.iloc[indics]
-    return data_batch.as_matrix(),label_batch.as_matrix()
+    if is_train:
+        label_batch = label.iloc[indics]
+        return data_batch.as_matrix(),label_batch.as_matrix()
+    else:
+        return data_batch.as_matrix()
 
+def get_train_data():
+    train = pickle.load(open('../dump/train.pkl', 'rb'))
+
+    train = train.drop(['start_time', 'geohashed_start_loc', 'geohashed_end_loc'], axis=1)
+
+    train = train.astype(np.float64)
+    train_label = train[['end_longitude', 'end_latitude']]
+
+    train_data = train.drop(['end_longitude', 'end_latitude'], axis=1)
+
+    return train_data,train_label
+
+def get_test_data():
+    test = pickle.load(open('../dump/test.pkl','rb'))
+
+    test = test.drop(['start_time','geohashed_start_loc'],axis = 1)
+
+    test = test.astype(np.float64)
+
+    return test.as_matrix()
 
 if __name__ == '__main__':
 
-    train = pickle.load(open('../dump/train.pkl','rb'))
+    train_data,train_label = get_train_data()
 
-    train = train.drop(['start_time','geohashed_start_loc','geohashed_end_loc'],axis = 1)
-
-    train = train.astype(np.float64)
-    train_label = train[['end_longitude','end_latitude']]
-
-    train_data = train.drop(['end_longitude','end_latitude'],axis = 1)
+    test_data = get_test_data()
 
     center_point = pickle.load(open('../dump/cluster_center.pkl','rb'))
+
 
 
 
@@ -110,11 +135,17 @@ if __name__ == '__main__':
     y = tf.placeholder(tf.float64,[None,output_dimension])
     output = construct_graph(x,center_point)
     with tf.name_scope('loss'):
-        loss = tf.reduce_sum(tf.add(tf.square(tf.sin((output[:,1]-y[:,1])/2)),tf.multiply(tf.cos(y[:,1]),tf.multiply(tf.square(tf.sin((output[:,0]-y[:,0])/2)),tf.cos(output[:,1])))))
+        R = tf.constant(earth_radius,dtype=tf.float64)
+        #loss = tf.reduce_sum(tf.add(tf.square(tf.sin((output[:,1]-y[:,1])/2)),tf.multiply(tf.cos(y[:,1]),tf.multiply(tf.square(tf.sin((output[:,0]-y[:,0])/2)),tf.cos(output[:,1])))))
+        loss = tf.reduce_sum(tf.multiply(R,tf.sqrt(tf.add(tf.pow(output[:,1]-y[:,1],2),tf.pow(tf.multiply((output[:,0]-y[:,0]),tf.cos((output[:,1]-y[:,1])/2)),2)))))
         tf.summary.scalar('loss',loss)
     with tf.name_scope('train'):
-        train_step = tf.train.AdamOptimizer(1e-4).minimize(loss)
-    #正确性咋衡量,毕竟差一个小数位就是错的,只能看Loss?
+        #global_step = tf.Variable(0,name='global_step')
+        #learning_rate = tf.train.exponential_decay(0.01,global_step,1000,0.96)
+        learning_rate = 1e-2
+        tf.train.MomentumOptimizer(learning_rate,momentum=0.9).minimize(loss)
+        train_step = tf.train.AdamOptimizer(learning_rate).minimize(loss)
+        #add_global_step = global_step.assign_add(1)
 
 
 
@@ -127,17 +158,27 @@ if __name__ == '__main__':
         saver = tf.train.Saver()
         #test_writer = tf.summary.FileWriter(FLAGS.summaries_dir + '/test')
 
-        for i in range(200000):
 
-            train,label = generate_random_batch(train_data,train_label,FLAGS.batch_size)
-            if i % 100 == 0:
-                # save the model
-                loss_ = sess.run([loss],feed_dict={x:train,y:label})
+        for i in range(50000):
+
+            train,label = generate_train_random_batch(train_data,train_label,FLAGS.batch_size)
+            if i % 500 == 0:
+
+                loss_  = sess.run([loss],feed_dict={x:train,y:label})
                 print('loss',loss_)
-            if i % 3000 == 0:
-                saver.save(sess, FLAGS.checkpoint+'model')
+
+            if i % 1000 == 0:
+                #test_regresssion = sess.run([output], feed_dict={x: test_data})
+                #print('predict', test_regresssion)
+                #saver.save(sess, FLAGS.checkpoint+'model')
+                pass
             _,summary = sess.run([train_step,merged],feed_dict={x: train, y: label})
             train_writer.add_summary(summary,i)
+
+            #sess.run([add_global_step],feed_dict={x: train, y: label})
+        test_regresssion = sess.run([output],feed_dict={x:test_data})
+        print('predict',test_regresssion)
+        pickle.dump(test_regresssion,open('../dump/predict.pkl','wb'),protocol=4)
 
 
 
